e833a4ab5f3886201cc501a64d55a571825e6c4a
John Ardosa <jardosa@live.com>
2023-08-22 08:02:01 +0800

    feat: test query

--- apps/nextjs-with-tailwind-storybook/app/about/page.tsx
+++ apps/nextjs-with-tailwind-storybook/app/about/page.tsx
@@ -2,11 +2,12 @@
 
 
 import { useQuery } from '@apollo/experimental-nextjs-app-support/ssr'
-import { PingDocument } from '@nx-nextjs-tailwind-storybook/data-access'
+import { PingDocument, PingQuery } from '@nx-nextjs-tailwind-storybook/data-access'
 import React from 'react'
 
 const About = () => {
-  const { data, loading } = useQuery(PingDocument)
+  const { data, loading } = useQuery<PingQuery>(PingDocument)
+  console.log(data?.ping)
   console.log(data, loading)
   return (
     <div className='text-2xl'>About</div>
